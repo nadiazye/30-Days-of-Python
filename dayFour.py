97ca96d14c61e39c49eb91869fe744527e854e4f
Nadia Rodriguez <nzr6@miami.edu>
2026-07-30 12:28:50 -0400

    continuing day 4

--- dayFour.py
+++ dayFour.py
@@ -383,17 +383,24 @@ print(company.find(first_gone))
 print(company.replace('Coding','Python'))
 
 #Change "Python for Everyone" to "Python for All" using the replace method or other methods.
+language_for = 'Python for Everyone'
+
+print(language_for.replace('Everyone','All'))
 
 #Split the string 'Coding For All' using space as the separator (split()) .
+print(company.split( ))
 
 #"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+tech_companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+print(tech_companies.split(','))
 
 #What is the character at index 0 in the string Coding For All.
-
+print(company[0])
 #What is the last index of the string Coding For All.
-
+print(company[-1])
 #What character is at index 10 in "Coding For All" string.
-
+print(companny[10])
 #Create an acronym or an abbreviation for the name 'Python For Everyone'.
 
 #Create an acronym or an abbreviation for the name 'Coding For All'.
